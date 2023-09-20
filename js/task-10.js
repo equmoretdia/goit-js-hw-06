7850c4680dacd8ddac1e1bc5742ef95335fc38f3
Serhiy Belinskyy <serhiy.belinskyy@gmail.com>
2023-09-21 00:26:01 +0200

    100%

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const number = document.querySelector('#controls input');
+const createButton = number.nextElementSibling;
+const destroyButton = createButton.nextElementSibling;
+const boxes = document.querySelector('#boxes');
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = number.value;
+  let initialSize = 30;
+  for (let i = 1; i <= amount; i++) {
+    const div = document.createElement('div');
+    boxes.append(div);
+    div.style.margin = '5px 0';
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.width = `${initialSize}px`;
+    div.style.height = `${initialSize}px`;
+    initialSize += 10;
+  }
+}
+
+function destroyBoxes() {
+  const divsToRemove = boxes.children;
+  while (divsToRemove.length > 0) {
+    divsToRemove[0].remove();
+  }
+}
